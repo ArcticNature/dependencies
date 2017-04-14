bfe0f94f970278b1490a000aa955764847081d2a
Stefano Pogliani <stefano@spogliani.net>
2017-04-14 11:46:56 +0100

    Add poolqueue promeses.

---
+++ poolqueue/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Compile static version of poolqueue.
+
+
+COMPONENT_PATH="${PWD}/${1}"
+BUILD_PATH="${COMPONENT_PATH}/build"
+SRC_PATH="${COMPONENT_PATH}/source"
+
+
+### MAIN ###
+mkdir -p "${BUILD_PATH}"
+cd "${SRC_PATH}"
+
+# If the no-clean file is missing clear everything first.
+if [ ! -f "${BUILD_PATH}/no-clean" ]; then
+  git reset --hard
+  git clean --force
+  ./bootstrap.sh
+  ./configure --prefix "${BUILD_PATH}"
+fi
+
+touch "${BUILD_PATH}/no-clean"
+make
+make install > install.log
